66e05d208dbc68616676fc6882e652c255a30903
Joonyoung <blackhound97@gmail.com>
2024-06-04 17:39:50 +0900

    1주차 백준 풀이

--- baekjoon/src/Main.java
+++ baekjoon/src/Main.java
@@ -1,22 +1,5 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int num = Integer.parseInt(br.readLine());
-        for(int i=0;i<num;i++) {
-            String[] input = br.readLine().split(" ");
-            int a = Integer.parseInt(input[0]);
-            int b = Integer.parseInt(input[1]);
-            bw.write((a + b) + "\n");
-        }
-        bw.flush();
-        bw.close();
-        br.close();
+    public static void main(String[] args) {
+
     }
 }
