d816968408c95db24f88f3a81625ba76585ce9c7
Joonyoung <blackhound97@gmail.com>
2024-06-09 10:36:55 +0900

    0609 일일 기본문제 풀이

---
+++ baekjoon/src/Basic/BOJ11718.java
@@ -0,0 +1,13 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class BOJ11718 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            String text = sc.nextLine();
+            System.out.println(text);
+        }
+    }
+}
---
+++ baekjoon/src/Basic/BOJ7287.java
@@ -0,0 +1,8 @@
+package Basic;
+
+public class BOJ7287 {
+    public static void main(String[] args) {
+        System.out.println(28);
+        System.out.println("joonymo");
+    }
+}
