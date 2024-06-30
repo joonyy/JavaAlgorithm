51aa25d05a87cfb80157ae4fc18f8ea34e77e505
Joonyoung <blackhound97@gmail.com>
2024-06-30 23:48:50 +0900

    0630 일일 기본문제 풀이

---
+++ baekjoon/src/Basic/BOJ1152.java
@@ -0,0 +1,14 @@
+package Basic;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class BOJ1152 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        StringTokenizer st= new StringTokenizer(s, " ");
+        System.out.println(st.countTokens());
+    }
+}
