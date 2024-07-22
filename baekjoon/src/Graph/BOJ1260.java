09ce4be06083f13b0ba8aae3505da31c878c1230
Joonyoung <blackhound97@gmail.com>
2024-07-22 21:43:08 +0900

    0721 일일 기본문제 풀이(디렉토리 변경)

---
+++ baekjoon/src/Graph/BOJ1260.java
@@ -0,0 +1,27 @@
+package Graph;
+
+import java.util.Scanner;
+
+//public class BOJ1260 {
+//    static int N;
+//    boolean[][] graph =new boolean[N][N];
+//    boolean[][] Visited = new boolean[N][N];
+//    public static boolean dfs(int a, int b){
+//
+//    }
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int N  = sc.nextInt();
+//        int M = sc.nextInt();
+//        int V = sc.nextInt();
+//
+//
+//        for(int i = 0 ; i<M; i++){
+//            int a = sc.nextInt();
+//            int b = sc.nextInt();
+//            graph[a-1][b-1] = true;
+//            graph[b-1][a-1] = true;
+//        }
+//
+//    }
+//}
